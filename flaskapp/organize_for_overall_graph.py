6457b83f192be98733354dd12d6a75ad3a78b1e5
Cori <coriw123@gmail.com>
2019-08-01 15:40:19 -0400

    added monthly data

--- flaskapp/organize_for_overall_graph.py
+++ flaskapp/organize_for_overall_graph.py
@@ -6,7 +6,7 @@ import us
 
 database = "all_data"
 #type of the data is always cc (#commoncore) or ccr ("college and career readiness")
-type_data = "cc"
+type_data = "ccr"
 table_from = type_data
 table_to = type_data + "_time_aggregated"
 
@@ -19,13 +19,13 @@ end_time = "2020"
 
 #make the cols for the time aggregated sentiment df
 time_ag_df = pd.DataFrame(
-	index = pd.date_range(start=start_time, end=end_time), 
+	index = pd.date_range(start=start_time, end=end_time, freq = "M").to_period("M"), 
 	columns = ['sentiment'])
 
 #make sure we're using full tweets (not deleted ones)
 old_df = old_df[old_df['deleted'] == 0]
 
-old_df['created_at'] = pd.to_datetime(old_df['created_at']).dt.round(freq = 'D')
+old_df['created_at'] = pd.to_datetime(old_df['created_at']).dt.to_period('M')
 
 #and then of the full tweets, make sure there aren't duplicates; since Prof. Rice's data doesn't have 
 #indices we just check time created and text of the tweet to see if they're the same
@@ -37,4 +37,4 @@ c.execute("DROP TABLE IF EXISTS %s" % table_to)
 time_ag_df.to_sql(table_to, con, index= False)
 con.close()
 
-time_ag_df.to_csv(type_data + "_overall.csv")
+time_ag_df.to_csv(type_data + "_overall_month.csv")
